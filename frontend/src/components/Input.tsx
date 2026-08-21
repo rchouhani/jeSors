0d464e38991103ee30c27eb53316281b09a78593
rchouhani <rchouhani@me.com>
2026-08-21 15:34:25 +0200

    add/ eye switch button to see or not password in input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { InputCompProps } from '../types/components';
 
 const InputComp = ({
@@ -14,18 +14,25 @@ const InputComp = ({
   keyboardType,
   secureTextEntry,
 }: InputCompProps) => {
-return (
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
   <View style={styles.wrapper}>
     {label && <Text style={[styles.labelInput, labelStyle]}>{label}</Text>}
     <View style={[styles.container, style]}>
       <TextInput
-          style={placeholderStyle}
+          style={styles.input}
           placeholder={placeholder}
           value={value}
           onChangeText={onChangeText}
           keyboardType={keyboardType}
-          secureTextEntry={secureTextEntry}
+          secureTextEntry={secureTextEntry && !isVisible}
       />
+      {secureTextEntry && (
+        <TouchableOpacity onPress={() => setIsVisible(!isVisible)}>
+            <Text>{isVisible ? '👁️' : '😣'}</Text>
+        </TouchableOpacity>
+      )}
       {icon && icon}
     </View>
   </View>
@@ -41,8 +48,14 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         backgroundColor: 'cornflowerblue',
         padding: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
       },
       labelInput: {
+        color: 'black',
+      },
+      input: {
+        flex: 1,
         color: 'black',
       }
 });
