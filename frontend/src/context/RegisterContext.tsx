1f7d495472647bcd4757960f7eff9b6bf2a533b2
rchouhani <rchouhani@me.com>
2026-08-22 12:20:57 +0200

    add/ iisLogin hook

--- frontend/src/context/RegisterContext.tsx
+++ frontend/src/context/RegisterContext.tsx
@@ -3,15 +3,18 @@ import React, { createContext, useContext, useState } from 'react';
 type RegisterContextType = {
     progress: number;
     setProgress: (value: number) => void;
+    isLogin: boolean;
+    setIsLogin: (value: boolean) => void;
 };
 
 const RegisterContext = createContext<RegisterContextType | null>(null);
 
 export const RegisterProvider = ({ children }: { children: React.ReactNode }) => {
     const [progress, setProgress] = useState(0);
+    const [isLogin, setIsLogin] = useState(true);
 
     return (
-        <RegisterContext.Provider value={{ progress, setProgress }}>
+        <RegisterContext.Provider value={{ progress, setProgress, isLogin, setIsLogin }}>
             {children}
         </RegisterContext.Provider>
     );
