cc7473a56499206640e701dc8542bbffa61b8c48
rchouhani <rchouhani@me.com>
2026-08-22 12:20:29 +0200

    update:/ bar position && bar display

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -3,11 +3,12 @@ import { View, StyleSheet } from 'react-native';
 import { useRegister } from '../context/RegisterContext';
 
 const ProgressBar = () => {
-    const { progress } = useRegister();
+    const { progress, isLogin } = useRegister();
+    if (!isLogin) return null;
 
     return (
         <View style={styles.container}>
-            <View style={[styles.bar, {width: `${progress}%` }]} />
+            {isLogin && <View style={[styles.bar, {width: `${progress}%` }]} /> }
         </View>
     );
 };
@@ -18,8 +19,8 @@ const styles = StyleSheet.create({
         height: 8,
         backgroundColor: '#e0e0e0',
         borderRadius: 4,
-        position: 'relative',
-        top: -100,
+        position: 'absolute',
+        top: 70,
     },
     bar: {
         height: 8,
